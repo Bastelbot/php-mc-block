1a4a8c176912305fa879ca651badba36ce2da6f8
Robert Böser <code@robertboeser.de>
2024-08-07 06:53:21 +0200

    patched ShortGrass

--- src/Minecraft/ShortGrass.php
+++ src/Minecraft/ShortGrass.php
@@ -1,7 +1,14 @@
 <?php
 namespace Bastelbot\McBlock\Minecraft;
 
-class ShortGrass extends Grass
+class ShortGrass extends BlockGrey
 {
+    protected $transparent = true;
 
+    public function initTexture ()
+    {
+        $tex = $this->loadTexture('grass');
+        $this->imagecolorize($tex, 0x00ffff00);
+        return $tex;
+    }
 }
